d9c3a02ef98b06a83f1488f2a4ff91aee1a45795
ElenaLj <elenaljubijankic@gmail.com>
2022-05-20 14:48:46 +0200

    :art: fixing accordion collapsed issue

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -11,13 +11,15 @@ export class ItemComponent implements OnInit {
   dateFormat = "EEEE d MMMM y"
   formattedDate = formatDate(this.currentDate, this.dateFormat, this.locale);
   messages: string[] = [
-    ""
+    "Hello, how are you?",
+    "Do you wanna go to the cinema tonight?",
+    "You are late! Where are you?"
   ]
 
   messageMapping: {[k: string]: string} = {
     "=0": $localize`:@@zero:No messages`,
     "=1": $localize`:@@one:One message`,
-    "other": $localize`:@@many:Many messages`
+    "other": $localize`:@@many:  # messages`
   }
 
   constructor(@Inject(LOCALE_ID) public locale: string) { }
