752464bdd7aa41d95b4280f19ea1ec0ee8d2fa19
ElenaLj <e.ljubijankic@codermine.com>
2022-07-04 17:55:08 +0200

    re-develop demo app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
-import { ItemComponent } from './item/item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardComponent,
-    ItemComponent
   ],
   imports: [
     BrowserModule
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,14 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
+export interface Pizza {
+  name: string,
+  quantity: number,
+  price: number
+}
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
-export class CardComponent implements OnInit {
+export class CardComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  pizzas: Pizza[] = [
+    {
+      name: $localize`:@@pizza-one:Pizza Hot Stuff`,
+      quantity: 2,
+      price: 18.90
+    },
+    {
+      name: $localize`:@@pizza-one:Pizza Classic Pepperoni`,
+      quantity: 3,
+      price: 28.50
+    },
+    {
+      name: $localize`:@@pizza-one:Pizza Very Veggie`,
+      quantity: 1,
+      price: 13.30
+    },
+    {
+      name: $localize`:@@pizza-one:Pizza Tropical Hawaiian`,
+      quantity: 1,
+      price: 15.90
+    },
+  ]
 }
