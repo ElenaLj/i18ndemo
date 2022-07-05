00817390e973b018b629f7dd7d44db994c986545
ElenaLj <e.ljubijankic@codermine.com>
2022-07-05 17:43:00 +0200

    WIP: add pipe

--- src/app/card/currencySymbol.pipe.ts
+++ src/app/card/currencySymbol.pipe.ts
@@ -1,14 +1,11 @@
 import { Inject, LOCALE_ID, Pipe, PipeTransform } from '@angular/core';
 
-// my pipe needs to print $ for locale en-US and € for it-IT
-
 @Pipe({
   name: 'currencySymbolPipe'
 })
 export class currencySymbolPipe implements PipeTransform {
 
   transform(value: any) {
-    // TODO
     if (this.locale == "en-US") {
       return "$" + value;
     }
